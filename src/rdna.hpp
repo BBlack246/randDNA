dfa0ce1b15cda8838d6ccd0598dce9b0829bf163
BBlack246 <BBlack246@users.noreply.github.com>
2019-02-21 15:01:14 -0400

    Added source file

---
+++ src/rdna.hpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include <random>
+
+using namespace std;
+string randDNA(int seed, string bases, int x)
+{	
+	mt19937 eng(seed);
+	string DNA;
+	
+	if (bases.size() == 0)
+	{
+		DNA= "";
+		return DNA;
+	}
+	
+	uniform_int_distribution<>un(0, bases.size()-1);
+	
+	for(int i=0 ;  i < x ;  i++) 
+	{ 
+		DNA+= bases[un(eng)];
+	}
+	return DNA; 
+}
